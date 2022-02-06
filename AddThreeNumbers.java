4625b2c2da81187256ebd4dc5a214f68d42ef360
Caesar M <melero.jr.231@gmail.com>
2022-02-05 18:21:58 -0800

    some message

---
+++ AddThreeNumbers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+/**
+* Will add three ints for user.
+* 1) Define vars
+* 2) Message to user
+* 3) Create a Scanner object
+* 4) Use Scanner object to get three integers from the user
+* 5) Output message and result
+*
+* @author Cesar Melero, melero.jr.231@gmail.com
+* @version a version number or a date (ex. v1.0)
+* @since last date of update (2022-2-4)
+*/
+public class AddThreeNumbers
+{
+public static void main (String [] args)
+{
+//input varaibles
+int n1, n2, n3;
+System.out.println ("Hello out there.");
+System.out.println ("I will add three numbers for you.");
+System.out.println ("Enter three whole numbers on a line:");
+//Gets input from user
+Scanner keyboard = new Scanner (System.in);
+n1 = keyboard.nextInt ();
+n2 = keyboard.nextInt ();
+n3 = keyboard.nextInt ();
+System.out.println ("The sum of those three numbers is: ");
+System.out.println (n1 + n2 + n3);
+}
+}
